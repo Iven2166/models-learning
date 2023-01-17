34b6af7f7eed30e7ba47b13b8e43e702b08433a3
Iven2166 <iven2166@163.com>
2023-01-17 09:58:06 +0800

    add

--- graph-models/GraphSAGE/graph_dgl/dgl_garphsage.py
+++ graph-models/GraphSAGE/graph_dgl/dgl_garphsage.py
@@ -1,4 +1,5 @@
 # 参考：https://zhuanlan.zhihu.com/p/344067462
+# https://zhuanlan.zhihu.com/p/348382635
 
 import torch as torch
 import dgl
@@ -11,7 +12,7 @@ import torch.nn.functional as F
 from dgl.nn.pytorch import SAGEConv
 
 
-class myGraphSAGE(nn.Module):
+class MyGraphSAGE(nn.Module):
     def __init__(self,
                  in_feats,
                  n_hidden,
@@ -20,5 +21,29 @@ class myGraphSAGE(nn.Module):
                  activation,
                  dropout,
                  aggregator):
-        super(myGraphSAGE, self).__init__()
+        super(MyGraphSAGE, self).__init__()
         self.n_layers = n_layers
+        self.n_hidden = n_hidden
+        self.n_classes = n_classes
+        self.layer = nn.ModuleList()
+        self.layer.append(SAGEConv(in_feats, n_hidden, aggregator))
+        for i in range(1, n_layers - 1):
+            self.layer.append(SAGEConv(n_hidden, n_hidden, aggregator))
+        self.layer.append(SAGEConv(n_hidden, n_classes, aggregator))
+        self.dropout = nn.Dropout(dropout)
+        self.activation = activation
+
+    def forward(self, blocks, feats):
+        """
+        前向计算函数
+        blocks:
+        feats:
+        """
+        h = feats # 应该是最初始输入的节点特征
+        for i, (layer, block) in enumerate(zip(self.layer, blocks)):
+            h = layer(block, h)
+            if i != self.n_layers - 1:
+                h = self.activation(h)
+                h = self.dropout(h)
+        return h
+

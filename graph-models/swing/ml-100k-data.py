5e5997a9b7d4a458a18ae60304d56b4054abb70e
lgz <iven2166@163.com>
2023-01-21 00:37:40 +0800

    modify

--- graph-models/swing/ml-100k-data.py
+++ graph-models/swing/ml-100k-data.py
@@ -3,5 +3,5 @@ import pandas as pd
 train_data = pd.read_csv('./ua.base', sep="\t", engine="python", names=["userid", "movieid", "rate", "event_timestamp"])
 # test_data = pd.read_csv(test_path, sep="\t", engine="python", names=["userid", "movieid", "rate", "event_timestamp"])
 
-print(train_data.loc[:, ['userid','movieid']])
-train_data.loc[:, ['userid','movieid']].to_csv('./ml-100k-uabase.csv')
+print(train_data.loc[:, ['userid','movieid','rate']])
+train_data.loc[:, ['userid','movieid','rate']].to_csv('./ml-100k-uabase.csv')
